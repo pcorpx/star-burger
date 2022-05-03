5cd59b160fd95fd67ad6fd6089b9ffed4a381eda
Полежаев Игорь Олегович <ingvar@kontur>
2022-05-03 16:43:25 +0500

    Add data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 from .models import Product, Order, OrderElement
@@ -59,21 +60,58 @@ def product_list_api(request):
         'indent': 4,
     })
 
+
 @api_view(['POST'])
 def register_order(request):
+    err_content = 'Invalid data was provided'
     try:
-        requested_order = json.loads(request.body.decode())
-        print(requested_order)
+        req_order = json.loads(request.body.decode())
+        print(req_order)
+        order_elem = {'product', 'quantity'}
+        if 'products' not in req_order:
+            err_content = 'There is no products list in order'
+            raise ValueError
+        if not isinstance(req_order['products'], list):
+            err_content = 'The products value should be a list'
+            raise ValueError
+        if not req_order['products']:
+            err_content = 'The products list can\'t be an empty'
+            raise ValueError
+        if any([not isinstance(req_product, dict)
+                or not order_elem.issubset(set(req_product.keys()))
+                for req_product in req_order['products']]):
+            err_content = 'Invalid products items in the list was provided'
+            raise ValueError
         created_order = Order.objects.create(
-            firstname=requested_order['firstname'],
-            lastname=requested_order['lastname'],
-            phone_number=requested_order['phonenumber'],
-            address=requested_order['address'],
+            firstname=req_order['firstname'],
+            lastname=req_order['lastname'],
+            phone_number=req_order['phonenumber'],
+            address=req_order['address'],
         )
-        for requested_product in requested_order['products']:
-            product_id = requested_product['product']
-            quantity = requested_product['quantity']
-            product = Product.objects.get(id=product_id)
+        for req_product in req_order['products']:
+            if isinstance(req_product['product'], str) and \
+               not req_product['product'].isdigit():
+                err_content = 'The product id should be a digit'
+                raise
+            if isinstance(req_product['product'], (str, int)) and \
+               Product.objects.filter(id=req_product['product']) \
+                              .exists():
+                product_id = req_product['product']
+                product = Product.objects.get(id=product_id)
+            else:
+                err_content = 'Non existent product was provided'
+                raise ValueError
+            if isinstance(req_product['quantity'], str) and \
+               not req_product['quantity'].isdigit():
+                err_content = 'The quantity should be a digit'
+                raise
+            if isinstance(req_product['product'], (str, int)) and \
+               int(req_product['quantity']) in range(1, 999):
+                quantity = req_product['quantity']
+            else:
+                err_content = 'Quantity of product should be between ' \
+                    '1 and 999'
+                raise ValueError
             OrderElement.objects.create(
                 order=created_order,
                 product=product,
@@ -82,6 +120,6 @@ def register_order(request):
         return Response({}, status=200)
     except ValueError:
         return Response(
-            {'error': 'Invalid value was provided'},
-            status=400
+            {'error': err_content},
+            status=status.HTTP_406_NOT_ACCEPTABLE
         )
