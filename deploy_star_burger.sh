9d7a0fac213ef6980f9553e204bbfec345a171f8
Igor Polezhaev <pcorp@mail.ru>
2022-07-29 10:00:01 +0000

    check for non commited migrations

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -4,9 +4,9 @@ set -e
 
 git -C /opt/star-burger pull
 /opt/star-burger/myvenv/bin/pip3 install -r /opt/star-burger/requirements.txt
+/opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py makemigrations --dry-run --check
 npm ci --dev --prefix /opt/star-burger
 /opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py collectstatic --noinput
-/opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py makemigrations
 /opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py migrate
 systemctl restart star-burger-web.target
 systemctl reload nginx
