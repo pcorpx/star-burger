31c326df1bab5fe5566ea0c38b837a5d88255fc3
Igor Polezhaev <pcorp@mail.ru>
2022-07-29 10:02:23 +0000

    supress user input when migrate

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -7,7 +7,7 @@ git -C /opt/star-burger pull
 /opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py makemigrations --dry-run --check
 npm ci --dev --prefix /opt/star-burger
 /opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py collectstatic --noinput
-/opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py migrate
+/opt/star-burger/myvenv/bin/python3.10 /opt/star-burger/manage.py migrate --noinput
 systemctl restart star-burger-web.target
 systemctl reload nginx
 http POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:$ROLLBAR_TOKEN environment=$ROLLBAR_ENV revision=$(git rev-parse --verify HEAD) rollbar_username=pcorpx comment="new deploy" status=succeeded
